2260337846da93a4d067bd8a4abf82f9eccab18c
dhuber666 <dhuber666@gmail.com>
2017-04-07 11:47:24 +0200

    Added a Change Size Button but not working

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
 
+$boardSize = $(".board").height();
+
+
+
+
 
 $("document").ready(function(){
 
 	
-
-    buildBox();
+	$loopCount = 256;
+    buildBox($loopCount);
+	hoverFunc();
+    $(".box").height(60);
+	$(".box").width(60);
 	
-    
-
-	$( ".box" ).hover(function() {
-    	$( this ).addClass( "hover" );
-  	});
-  
+	
 
 	
 
 });
 
+function hoverFunc() {
+
+	$( ".box" ).hover(function() {
+			$( this ).addClass( "hover" );
+		});
+  
+
+};
 
 
 
 
+function buildBox(count) {
 
-function buildBox() {
-
-    for(i = 0; i < 256; i++){
+    for(i = 0; i < count; i++){
 
 		$(".board").append("<div class='box'></div>");
 
 		
 
 	}
+};
+
+function removeBox() {
+
+	$(".box").remove();
+}
+
+function changeSize() {
+
+	$size = prompt("Change the size!");
+	$divSize = $boardSize / $size;
+
+	$(".box").height($divSize);
+	$(".box").width($divSize);
+
+	$loopCount = $size * $size;
+	removeBox();
+	buildBox($loopCount);
+	hoverFunc();
+
+
+
+
 };
 
